e12701821fc2d222f68840a5e8dc9d339347a609
Rohini <h.rohini19@gmail.com>
2025-06-03 17:56:41 +0530

    Script file added

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the data from the form
+const name = document.querySelector("#name");
+const id = document.querySelector("#id");
+const email = document.querySelector("#email");
+const contact = document.querySelector("#contact");
+
+// Get the button and display area elements
+const button = document.querySelector(".btn");
+const displayArea = document.querySelector(".displayArea");
+
+// Add row when the butto is clicked
+button.addEventListener('click', () => {
+    console.log("button clicked")
+});
